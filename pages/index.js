238beb2622bc0815954b95f111c4ed68408ab15d
angeldev <angeldev@angel.com>
2020-09-19 01:46:44 -0500

    cambio prueba 3

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home(){
     <Layout>
       <Title>Home page</Title>
       <p className='ha'>Mi primer pagina con Next.js</p>
+      <img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fshitpostosiosi%2F&psig=AOvVaw1s5sPSZ_719jenZ7vYWo9j&ust=1600584140727000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCZid7O9OsCFQAAAAAdAAAAABAJ"></img>
 
       <style>
         {`
