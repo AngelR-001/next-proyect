d6283e077caade646ba760386fc2e2bf75338916
angeldev <angeldev@angel.com>
2020-09-19 01:55:55 -0500

    cambio prueba 5

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Title from '../components/Title';
 import Layout from '../components/layout';
 
+
 export default function Home(){
    return(
     <Layout>
       <Title>Home page</Title>
       <p className='ha'>Mi primer pagina con Next.js</p>
-      <img src = "prueba.jpg"/>
+      <img src = {prueba.jpg}/>
 
       <style>
         {`
